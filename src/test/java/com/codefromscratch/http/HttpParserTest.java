ef5bf2a0fa7a2a6acdc7993da04c1bc7cd9afaa5
ciubotarum <ciubotarum@outlook.com>
2024-09-17 10:58:14 +0300

    Generate test cases for invalid get request

--- src/test/java/com/codefromscratch/http/HttpParserTest.java
+++ src/test/java/com/codefromscratch/http/HttpParserTest.java
@@ -11,6 +11,7 @@ import java.nio.charset.StandardCharsets;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HttpParserTest {
     private HttpParser httpParser;
@@ -28,10 +29,57 @@ class HttpParserTest {
                     generateValidGETTestCase()
             );
         } catch (HttpParsingException e) {
-           fail(e);
+            fail(e);
         }
 
         assertEquals(request.getMethod(), HttpMethod.GET);
+    }
+
+
+    @Test
+    void parseHttpRequestBadMethod1() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(
+                    generateBadTestCaseMethodName1()
+            );
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED);
+        }
+    }
+
+    @Test
+    void parseHttpRequestBadMethod2() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(
+                    generateBadTestCaseMethodName2()
+            );
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED);
+        }
+    }
+    @Test
+    void parseHttpRequestInvNumItems1() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(
+                    generateBadTestCaseRequestLineInvNumItems1()
+            );
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+    }
+    @Test
+    void parseHttpEmptyRequestLine() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(
+                    generateBadTestCaseEmptyRequestLine()
+            );
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
     }
 
     private InputStream generateValidGETTestCase() {
@@ -78,15 +126,47 @@ class HttpParserTest {
         return inputStream;
     }
 
-    @Test
-    void parseHttpRequestBadMethod1() {
-        try {
-            HttpRequest request = httpParser.parseHttpRequest(
-                    generateBadTestCaseMethodName1()
-            );
-            fail();
-        } catch (HttpParsingException e) {
-            assertEquals(e.getErrorCode(), HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED);
-        }
+    private InputStream generateBadTestCaseMethodName2() {
+        String rawData = "GETTTT / HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-GB,en;q=0.9,en-US;q=0.8\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(
+                rawData.getBytes(
+                        StandardCharsets.US_ASCII
+                )
+        );
+
+        return inputStream;
+    }
+
+    private InputStream generateBadTestCaseRequestLineInvNumItems1() {
+        String rawData = "GET / AAAAAA HTTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-GB,en;q=0.9,en-US;q=0.8\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(
+                rawData.getBytes(
+                        StandardCharsets.US_ASCII
+                )
+        );
+
+        return inputStream;
+    }
+    private InputStream generateBadTestCaseEmptyRequestLine() {
+        String rawData = "\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Accept-Language: en-GB,en;q=0.9,en-US;q=0.8\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(
+                rawData.getBytes(
+                        StandardCharsets.US_ASCII
+                )
+        );
+
+        return inputStream;
     }
 }
