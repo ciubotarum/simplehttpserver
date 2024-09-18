582687cbbd3b743443cc57d20974d0d5d39f9e22
ciubotarum <ciubotarum@outlook.com>
2024-09-18 10:35:42 +0300

    Add getters and setters for header and version

--- src/main/java/com/codefromscratch/http/HttpRequest.java
+++ src/main/java/com/codefromscratch/http/HttpRequest.java
@@ -1,9 +1,14 @@
 package com.codefromscratch.http;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class HttpRequest extends HttpMessage{
     private HttpMethod method;
     private String requestTarget;
-    private String httpVersion;
+    private String originalHttpVersion;  // literal from the request
+    private HttpVersion bestCompatibleHttpVersion;
+    private HashMap<String, String> headers = new HashMap<>();
 
     HttpRequest() {
 
@@ -11,6 +16,23 @@ public class HttpRequest extends HttpMessage{
 
      public HttpMethod getMethod() {
         return method;
+    }
+    public String getRequestTarget() {
+        return requestTarget;
+    }
+
+    public HttpVersion getBestCompatibleHttpVersion() {
+        return bestCompatibleHttpVersion;
+    }
+
+    public String getOriginalHttpVersion() {
+        return originalHttpVersion;
+    }
+    public Set<String> getHeaderNames() {
+        return headers.keySet();
+    }
+    public String getHeader(String headerName) {
+        return headers.get(headerName.toLowerCase());
     }
 
     void setMethod(String methodName) throws HttpParsingException {
@@ -23,5 +45,26 @@ public class HttpRequest extends HttpMessage{
         throw new HttpParsingException(
                 HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED
         );
+    }
+
+    public void setRequestTarget(String requestTarget) throws HttpParsingException {
+        if (requestTarget == null || requestTarget.length() == 0) {
+            throw new HttpParsingException(HttpStatusCode.SERVER_ERROR_500_INTERNAL_SERVER_ERROR);
+        }
+        this.requestTarget = requestTarget;
+    }
+
+    void setHttpVersion(String originalHttpVersion) throws BadHttpVersionException, HttpParsingException {
+        this.originalHttpVersion = originalHttpVersion;
+        this.bestCompatibleHttpVersion = HttpVersion.getBestCompatibleVersion(originalHttpVersion);
+        if (this.bestCompatibleHttpVersion == null) {
+            throw new HttpParsingException(
+                    HttpStatusCode.SERVER_ERROR_505_HTTP_VERSION_NOT_SUPORTED
+            );
+        }
+    }
+
+    void addHeader(String headerName, String headerField) {
+        headers.put(headerName.toLowerCase(), headerField);
     }
 }
