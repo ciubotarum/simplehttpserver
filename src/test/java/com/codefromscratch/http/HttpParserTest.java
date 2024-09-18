a09482da01a0508f7d2705b738bb51fd623bef02
ciubotarum <ciubotarum@outlook.com>
2024-09-18 10:34:34 +0300

    Add tests for bad version

--- src/test/java/com/codefromscratch/http/HttpParserTest.java
+++ src/test/java/com/codefromscratch/http/HttpParserTest.java
@@ -33,6 +33,8 @@ class HttpParserTest {
         assertNotNull(request);
         assertEquals(request.getMethod(), HttpMethod.GET);
         assertEquals(request.getRequestTarget(), "/");
+        assertEquals(request.getOriginalHttpVersion(), "HTTP/1.1");
+        assertEquals(request.getBestCompatibleHttpVersion(), HttpVersion.HTTP_1_1);
     }
     @Test
     void parseHttpRequestBadMethod1() {
@@ -87,6 +89,41 @@ class HttpParserTest {
             fail();
         } catch (HttpParsingException e) {
             assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+    }
+    @Test
+    void parseHttpRequestBadHttpVersion() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(
+                    generateBadHttpVersionTestCase()
+            );
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+    }
+    @Test
+    void parseHttpRequestUnsupportedHttpVersion() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(
+                    generateUnsupportedHttpVersionTestCase()
+            );
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.SERVER_ERROR_505_HTTP_VERSION_NOT_SUPORTED);
+        }
+    }
+    @Test
+    void parseHttpRequestSupportedHttpVersion1() {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(
+                    generateSupportedHttpVersion1()
+            );
+            assertNotNull(request);
+            assertEquals(request.getBestCompatibleHttpVersion(), HttpVersion.HTTP_1_1);
+            assertEquals(request.getOriginalHttpVersion(), "HTTP/1.2");
+        } catch (HttpParsingException e) {
+            fail();
         }
     }
 
@@ -181,6 +218,90 @@ class HttpParserTest {
         String rawData = "GET /  HTTP/1.1\r" +   // <---------- no LF (line field)
                 "Host: localhost:8080\r\n" +
                 "Accept-Language: en-GB,en;q=0.9,en-US;q=0.8\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(
+                rawData.getBytes(
+                        StandardCharsets.US_ASCII
+                )
+        );
+
+        return inputStream;
+    }
+    private InputStream generateBadHttpVersionTestCase() {
+        String rawData = "GET / HTP/1.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cache-Control: max-age=0\r\n" +
+                "sec-ch-ua: \"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Microsoft Edge\";v=\"128\"\r\n" +
+                "sec-ch-ua-mobile: ?0\r\n" +
+                "sec-ch-ua-platform: \"Windows\"\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Sec-Fetch-Dest: document\r\n" +
+                "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
+                "Accept-Language: en-GB,en;q=0.9,en-US;q=0.8\r\n" +
+                "Cookie: __stripe_mid=cf756807-0bb4-4584-82b5-e45a7844e9874be3bc\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(
+                rawData.getBytes(
+                        StandardCharsets.US_ASCII
+                )
+        );
+
+        return inputStream;
+    }
+    private InputStream generateUnsupportedHttpVersionTestCase() {
+        String rawData = "GET / HTTP/2.1\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cache-Control: max-age=0\r\n" +
+                "sec-ch-ua: \"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Microsoft Edge\";v=\"128\"\r\n" +
+                "sec-ch-ua-mobile: ?0\r\n" +
+                "sec-ch-ua-platform: \"Windows\"\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Sec-Fetch-Dest: document\r\n" +
+                "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
+                "Accept-Language: en-GB,en;q=0.9,en-US;q=0.8\r\n" +
+                "Cookie: __stripe_mid=cf756807-0bb4-4584-82b5-e45a7844e9874be3bc\r\n" +
+                "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(
+                rawData.getBytes(
+                        StandardCharsets.US_ASCII
+                )
+        );
+
+        return inputStream;
+    }
+    private InputStream generateSupportedHttpVersion1() {
+        String rawData = "GET / HTTP/1.2\r\n" +
+                "Host: localhost:8080\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Cache-Control: max-age=0\r\n" +
+                "sec-ch-ua: \"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Microsoft Edge\";v=\"128\"\r\n" +
+                "sec-ch-ua-mobile: ?0\r\n" +
+                "sec-ch-ua-platform: \"Windows\"\r\n" +
+                "Upgrade-Insecure-Requests: 1\r\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+                "Sec-Fetch-Site: none\r\n" +
+                "Sec-Fetch-Mode: navigate\r\n" +
+                "Sec-Fetch-User: ?1\r\n" +
+                "Sec-Fetch-Dest: document\r\n" +
+                "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
+                "Accept-Language: en-GB,en;q=0.9,en-US;q=0.8\r\n" +
+                "Cookie: __stripe_mid=cf756807-0bb4-4584-82b5-e45a7844e9874be3bc\r\n" +
                 "\r\n";
 
         InputStream inputStream = new ByteArrayInputStream(
